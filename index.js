b165c2a50f66a4184b26b7eff5f83680e54d75b8
kate <kateincoding@gmail.com>
2022-06-01 04:53:52 -0500

    first express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My App");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello express');
+});
+
+app.listen(port, () => {
+  console.log("I am listening on the port" + port);
+});
