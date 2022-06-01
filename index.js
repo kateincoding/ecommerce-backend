763fa3ace2b73d65ce686503dd922de3087034c6
kate <kateincoding@gmail.com>
2022-06-01 12:49:50 -0500

    [routing]

--- index.js
+++ index.js
@@ -7,10 +7,39 @@ app.get('/', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  res.json({
-    name: "Tofu",
-    price: 10,
-  })
+  res.json([
+    {
+      name: "Tofu",
+      price: 10,
+    },
+    {
+      name: "Tofu",
+      price: 10,
+    }
+    ]);
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json(
+    {
+      id,
+      name: "Tofu",
+      price: 10,
+    }
+  )
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json(
+    {
+      categoryId,
+      productId,
+      name: "Tofu",
+      price: 10,
+    }
+  )
 })
 
 app.listen(port, () => {
