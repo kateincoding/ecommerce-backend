ead1cd4901bee4b1c89856058253b55f8a75a31d
kate <kateincoding@gmail.com>
2022-06-01 04:59:55 -0500

    [routing]

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello express');
 });
 
+app.get('/products', (req, res) => {
+  res.json({
+    name: "Tofu",
+    price: 10,
+  })
+})
+
 app.listen(port, () => {
   console.log("I am listening on the port" + port);
 });
