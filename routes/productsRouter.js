ef388717c76d2332ab560dd8d7d0e77e227f9b7b
kate <kateincoding@gmail.com>
2022-06-06 18:50:08 -0500

    [services implemented]

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const { append } = require('express/lib/response');
-const faker = require('faker');
-
+const ProductService = require('../services/productService');
 const router = express.Router();
+const service = new ProductService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.find();
   res.json(products);
-})
+});
 
 // los endpoint especifico van antes del dinamico
 router.get('/filter', (req, res) => {
@@ -25,13 +16,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json(
-    {
-      id,
-      name: "Tofu",
-      price: 10,
-    }
-  )
+  const product = service.findOne(id);
+  res.json(product);
 })
 
 router.post('/', (req, res) => {
